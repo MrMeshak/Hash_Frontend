cd0260efdefe61f6054c5f1fe69e0383352d99c5
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2022-10-03 21:21:37 +1100

    Updated backend Url

--- services/authApi.ts
+++ services/authApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { IUser } from '../store/user/userModel';
 
 export const axiosAuth = axios.create({
-  baseURL: 'https://hashbackend-production.up.railway.app:4000/auth',
+  baseURL: '104.196.232.237:4000',
   timeout: 8000,
   withCredentials: true
 });
